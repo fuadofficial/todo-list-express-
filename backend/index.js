d0bb3c758fd7372a9cec0ee6caaa454fd0060eb4
fuad <ahmadfuadoorakam@gmail.com>
2024-08-19 20:07:41 +0530

    created server in get method

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors());
+app.use(express.json())
+
+const todoList = [
+    {
+        id: '1',
+        todo: 'I phone',
+        isCompleted: false,
+    },
+]
 
 app.get('/api/todo', (req, res) => {
-    res.json("hy get methord")
+    res.status(200).json(todoList)
+})
+app.all('*', (req, res) => {
+    res.status(404).json("This page is not found")
 })
 
 // app.post('/api/todo', (req, res) => {
@@ -19,5 +33,5 @@ app.get('/api/todo', (req, res) => {
 // })
 
 
-const PORT = 5000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`server is running on ${PORT}`))
