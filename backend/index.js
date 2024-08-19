616426fa463ac950a87734e8e739fa23630d1d19
fuad <ahmadfuadoorakam@gmail.com>
2024-08-19 18:29:28 +0530

    some

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.json("hy")
+
+app.get('/api/todo', (req, res) => {
+    res.json("hy get methord")
 })
+
+// app.post('/api/todo', (req, res) => {
+//     res.json("hy")
+// })
+
+// app.put('/api/todo', (req, res) => {
+//     res.json("hy")
+// })
+
+// app.delete('/api/todo', (req, res) => {
+//     res.json("hy")
+// })
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`server is running on ${PORT}`))
