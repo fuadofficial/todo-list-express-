5985a686aa9b11be554db9bdfdda43c2bda7f732
fuad <ahmadfuadoorakam@gmail.com>
2024-08-19 20:30:18 +0530

    postman post

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+import { v4 as uuidv4 } from 'uuid';
 
 app.use(cors());
 app.use(express.json())
@@ -20,9 +21,19 @@ app.all('*', (req, res) => {
     res.status(404).json("This page is not found")
 })
 
-// app.post('/api/todo', (req, res) => {
-//     res.json("hy")
-// })
+app.post('/api/todo', (req, res) => {
+    const { todo } = req.body;
+    const todoItem = [
+        {
+            id: uuidv4(),
+            todo: todo,
+            isCompleted: false
+        }
+    ]
+    todoList.push(todoItem)
+    res.json(todoList)
+});
+
 
 // app.put('/api/todo', (req, res) => {
 //     res.json("hy")
