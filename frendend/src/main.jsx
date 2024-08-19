31a783ce9efb8c2751e4157ed425605f6047a9a1
fuad <ahmadfuadoorakam@gmail.com>
2024-08-19 17:35:32 +0530

    added some backend codes

--- frendend/src/main.jsx
+++ frendend/src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./style/global.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
