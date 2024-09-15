66ac931bfb7745293ba9ee50f7f36f7ba0954ef0
fuad <ahmadfuadoorakam@gmail.com>
2024-09-15 07:03:45 +0530

    database work completed successfully

--- backend/model/todoModel.js
+++ backend/model/todoModel.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 
-const todoSchema = mongoose.Schema({
-    todo: {
-        type: String,
-        minLength: 3,
-        maxLength: 20,
-        required: true
+const todoSchema = mongoose.Schema(
+    {
+        todo: {
+            type: String,
+            minLength: 3,
+            maxLength: 20,
+            required: true
+        },
+        isCompleted: {
+            type: Boolean,
+            required: true
+        },
     },
-    isCompleted: {
-        type: Boolean,
-        required: true
-    },
-})
+    {
+        timestamps: true,
+        strict: false
+    }
+)
 
 module.exports = mongoose.model('Todo', todoSchema) 
--- backend/router/todo.js
+++ backend/router/todo.js
@@ -3,26 +3,22 @@ const router = express.Router()
 const Todo = require('../model/todoModel')
 
 router.get('/', async (req, res) => {
-    const todoList = await Todo.find()
+    const todoList = await Todo.find().select('todo isCompleted ');
+
     res.status(200).json(todoList);
 });
 
 router.post('/', async (req, res) => {
     try {
-        // const { todo } = req.body;
 
-        // Check if the required "todo" field exists in the request body
+        const newTodo = await Todo.create(req.body);
 
-        // if (!todo) {
-        //     return res.status(400).json({
-        //         message: `Invalid request: Required attribute "todo" is missing.`,
-        //     });
-        // }
+        if (newTodo) {
+            return res.status(201).json('Todo item added successfully');
+        }
 
-        // Create a new todo item in the database
-        await Todo.create(req.body);
+        res.status(404).json('Todo item not found')
 
-        res.status(201).json('Todo item added successfully');
     } catch (error) {
         res.status(400).json({
             message: error.message
@@ -30,47 +26,51 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/', async (req, res) => {
+    try {
+        const { _id, todo, isCompleted } = req.body;
 
-router.put('/', (req, res) => {
-    const { id, todo, isCompleted } = req.body;
-    const requiredFields = ['id', 'todo', 'isCompleted'];
-    const missingFields = [];
-
-    requiredFields.forEach(field => {
-        if (req.body[field] === undefined || req.body[field] === '') {
-            missingFields.push(field);
+        const fieldToUpdate = {
+            todo,
+            isCompleted
         }
-    });
 
-    if (missingFields.length) {
-        return res.status(400).json({
-            message: missingFields.map(field => `Field '${field}' is required`).join(', '),
-        });
+        const updateData = await Todo.findByIdAndUpdate(_id, fieldToUpdate, { new: true })
+
+        if (updateData) {
+            return res.status(200).json(updateData)
+        }
+
+        res.status(400).json({
+            message: `item with id: ${id} does not exist for update`
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
     }
-
-    const index = todoList.findIndex((item) => item.id === id);
-    if (index === -1) {
-        return res.status(404).json({
-            message: `Item with id: ${id} doesn't exist.`,
-        });
-    }
-
-    todoList[index].todo = todo;
-    todoList[index].isCompleted = isCompleted;
-
-    res.status(200).json(todoList);
 });
 
-router.delete('/', (req, res) => {
-    const { id } = req.body;
-    const todoIndex = todoList.findIndex((item) => item.id === id);
-    if (todoIndex !== -1) {
-        todoList.splice(todoIndex, 1);
-        return res.json(todoList);
+router.delete('/', async (req, res) => {
+    try {
+        const { _id } = req.body;
+
+        const deletedField = await Todo.findByIdAndDelete(_id)
+
+        if (deletedField) {
+            return res.status(200).json({
+                message: "Deleted Successfully"
+            });
+        }
+        res.status(404).json({
+            message: `Item : ${id} , doesn't exist for delete`,
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
     }
-    res.status(404).json({
-        message: `Item : ${id} , doesn't exist`,
-    });
 });
 
 module.exports = router
