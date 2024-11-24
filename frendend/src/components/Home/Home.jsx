95d184e5ecc1f98358c14a7b9ff449dd6ba41f8d
unknown <ahmadfuadoorakam@gmail.com>
2024-11-24 22:07:15 +0530

    resetup to mern without vercel

--- frendend/src/components/Home/Home.jsx
+++ frendend/src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./Home.css";
 
-const API_URL = "https://todo-list-express-147g-lgu3ibwmp-ahmad-fuads-projects.vercel.app/api/todo"
+const API_URL = "http://localhost:3000/api/todo"
 
 const Home = () => {
     const [inputValue, setInputValue] = useState("");
